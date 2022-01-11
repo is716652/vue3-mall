cd0f11d805abebe0649c4966d0093505c5223025
is716652 <is716652@qq.com>
2022-01-11 23:54:46 +0800

    one

--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,12 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+import vant from 'vant';
+import 'vant/lib/index.css'; //引入vant样式组件
+
+import axios from './axios/axiosConfig';
+
+const app = createApp(App);
+app.config.globalProperties.$http=axios; //挂载到项目上
+
+app.use(store).use(router).use(vant).mount('#app')
---
+++ src/test/test.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+    </div>
+</template>
+<script lang="ts">
+import {defineComponent, getCurrentInstance} from 'vue';
+export default defineComponent({
+    name:"test",
+    setup(){ 
+        const {proxy} : any= getCurrentInstance();
+
+        //发送ajax请求
+        function ajaxTest(){
+            proxy.$http.get('https://api.xdclass.net/pub/api/v1/web/index_card')
+            .then((res:any)=>{
+                console.log(res);
+            })
+            .catch((error:any)=>{
+                console.log(error);
+            })
+        }
+
+        ajaxTest()
+    }
+})
+</script>
+<style scoped>
+</style>
